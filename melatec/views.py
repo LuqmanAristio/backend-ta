8e6b7e255598056a693d02da33553258bc5d7cf8
LuqmanAristio <aristio170802@gmail.com>
2024-03-01 15:25:09 +0700

    update feat prediction endpoint

--- melatec/views.py
+++ melatec/views.py
@@ -14,6 +14,8 @@ import csv
 import subprocess
 import librosa
 import vamp
+from pydub import AudioSegment
+from pydub.silence import detect_silence
 import mimetypes
 
 @csrf_exempt
@@ -23,8 +25,19 @@ def predict_view(request):
         model_path = '/home/luqmanaristio/Tugas Akhir/backend-ta/melatec/model/modelfinal2.h5'
         model = load_model(model_path)
 
-        input_data = json.loads(request.body.decode('utf-8')).get('input_data')
-        data_uji = np.expand_dims(input_data, axis=1)
+        audio_file = request.FILES.get('audio_file')
+        audio = AudioSegment.from_file(audio_file, format="wav")
+
+        processed_audio = cut_and_limit_duration(audio, target_duration=120000)
+        processed_audio_path = '/home/luqmanaristio/Tugas Akhir/backend-ta/audio_cut.wav'
+        processed_audio.export(processed_audio_path, format="wav")
+
+        mfcc_result = extract_mfcc_from_audio(processed_audio_path)
+        mfcc_result = {key: float(value) for key, value in mfcc_result.items()}
+        mfcc_result_list = list(mfcc_result.values())
+
+        input_model = [mfcc_result_list]
+        data_uji = np.expand_dims(input_model, axis=0)
 
         prediction = model.predict(data_uji)
         prediction_list = prediction.tolist()
@@ -33,6 +46,42 @@ def predict_view(request):
 
     except Exception as e:
         return JsonResponse({'error': str(e)})
+
+def cut_and_limit_duration(audio, target_duration=120000):  
+    silence_ranges = detect_silence(audio, silence_thresh=-40, seek_step=1)
+
+    if silence_ranges:
+        start_time = silence_ranges[0][1]
+        trimmed_audio = audio[start_time:]
+
+        return trimmed_audio[:target_duration]
+
+    else:
+        print("Tidak ada bagian hening di awal yang terdeteksi.")
+        return audio[:target_duration]
+
+def extract_mfcc_from_audio(audio_path):
+    try:
+        y, sr = librosa.load(audio_path, sr=22050)
+        mfcc_mean, mfcc_var = calculate_mfcc(y, sr)
+
+        mfcc_result = {}
+        for i in range(1, 21):
+            mfcc_result[f'Mean_MFCC_{i}'] = mfcc_mean[i-1]
+            mfcc_result[f'Var_MFCC_{i}'] = mfcc_var[i-1]
+
+        return mfcc_result
+
+    except Exception as e:
+        y, sr = librosa.load(audio_path, sr=None)
+        return {'error': str(e)}
+
+def calculate_mfcc(y, sr):
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20)
+    mean_values = [mfccs[i, :].mean() for i in range(20)]
+    variance_values = [mfccs[i, :].var() for i in range(20)]
+
+    return mean_values, variance_values
 
 @csrf_exempt
 @require_POST
